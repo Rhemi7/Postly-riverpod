74572cce520ace9db618277ff8ad8bb64b45ec00
Rhemi7 <adelefatai@yahoo.com>
2021-12-22 16:07:27 +0100

    refactored user notifier

--- lib/notifier/user_notifier/user_notifier.dart
+++ lib/notifier/user_notifier/user_notifier.dart
@@ -1,32 +1,39 @@
 import 'dart:math';
 
+import 'package:Postly/core/usecases/usecase.dart';
 import 'package:Postly/data/repository/data_repository/user_services.dart';
 import 'package:Postly/data/repository/database/hive_repository.dart';
 import 'package:Postly/features/user/data/models/user/user.dart';
+import 'package:Postly/features/user/domain/usecases/get_user.dart';
 import 'package:Postly/notifier/user_notifier/user_state.dart';
 import 'package:Postly/utils/constants.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserNotifier extends StateNotifier<UserState> {
-  UserServices _userServices;
-  HiveRepository _hiveRepository;
+  GetUser getPostlyUser;
+  // HiveRepository _hiveRepository;
   User _user;
   User get user => _user;
-  UserNotifier(this._userServices, this._hiveRepository) : super(UserInitial());
+  UserNotifier(this.getPostlyUser) : super(UserInitial());
 
   Future<void> getUser() async {
     try {
       state = UserLoading();
-      _user = _hiveRepository.get<User>(key: kUser, name: kUserBox);
-      if (_user == null) {
-        final List<User> users = await _userServices.getUsers();
-        int index = Random().nextInt(users.length);
-        _user = users[index];
-        _hiveRepository.add<User>(name: kUserBox, key: kUser, item: _user);
+      final result = await getPostlyUser(NoParams());
+      result.fold((error) => state = UserError(message: "Error"), (user) {
+        _user = user;
         state = UserLoaded(user: _user);
-      } else {
-        state = UserLoaded(user: _user);
-      }
+      });
+      // _user = _hiveRepository.get<User>(key: kUser, name: kUserBox);
+      // if (_user == null) {
+      //   final List<User> users = await _userServices.getUsers();
+      //   int index = Random().nextInt(users.length);
+      //   _user = users[index];
+      //   _hiveRepository.add<User>(name: kUserBox, key: kUser, item: _user);
+      //   state = UserLoaded(user: _user);
+      // } else {
+      //   state = UserLoaded(user: _user);
+      // }
     } catch (e) {
       state = UserError(message: e.toString());
     }
